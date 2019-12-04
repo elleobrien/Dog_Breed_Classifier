2376c80a91a82fff342b06c0db5b54a152ff0169
Elle O'Brien <andronovhopf@gmail.com>
2019-12-04 11:36:31 -0800

    Update grow_dog_dataset.sh

--- grow_dog_dataset.sh
+++ grow_dog_dataset.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # This script grows the dog dataset.
-cd Stanford_Dogs/n02088094-Afghan_hound
-cp `ls | tail -100` ../../data  
+(cd Images/n02088094-Afghan_hound && cp `ls | tail -100` ../../data)  
 
-cd ../n02085936-Maltese_dog       
-cp `ls | tail -100` ../../data   
+(cd Images/n02085936-Maltese_dog && cp `ls | tail -100` ../../data)  
 
