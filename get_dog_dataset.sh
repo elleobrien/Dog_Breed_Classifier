563513bc339f6b097738b5cfeb2e73cfc1322422
Elle O'Brien <andronovhopf@gmail.com>
2019-12-04 11:29:16 -0800

    Update get_dog_dataset.sh

--- get_dog_dataset.sh
+++ get_dog_dataset.sh
@@ -8,13 +8,10 @@ tar -xczf images.tar
 # Keep only the breeds we care about. Get the first 100 images in those folders.
 mkdir data
 
-cd Images/n02088094-Afghan_hound
-cp `ls | head -100` ../../data      
+(cd Images/n02088094-Afghan_hound && cp `ls | head -100` ../../data)     
 
-cd ../n02085936-Maltese_dog       
-cp `ls | head -100` ../../data   
+(cd ../n02085936-Maltese_dog && cp `ls | head -100` ../../data)
 
-# Remove all the other dog breed folders
-cd ../..
+# Remove the tar file
 rm images.tar
 
