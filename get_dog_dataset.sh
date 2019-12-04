62000cce0a280c447780bdd389d2858b4909fcd9
Elle O'Brien <andronovhopf@gmail.com>
2019-12-04 11:35:49 -0800

    Update get_dog_dataset.sh

--- get_dog_dataset.sh
+++ get_dog_dataset.sh
@@ -10,7 +10,7 @@ mkdir data
 
 (cd Images/n02088094-Afghan_hound && cp `ls | head -100` ../../data)     
 
-(cd ../n02085936-Maltese_dog && cp `ls | head -100` ../../data)
+(cd Images/n02085936-Maltese_dog && cp `ls | head -100` ../../data)
 
 # Remove the tar file
 rm images.tar
